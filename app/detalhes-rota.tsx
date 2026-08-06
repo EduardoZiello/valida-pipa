1d7845849e245c62883a7d5954db166d3a739563
Eduardo Melo <eduardo.ziello@gmail.com>
2026-08-05 22:03:13 -0300

    feat: adiciona tela completa de detalhes da rota e melhora interface do aplicativo

--- app/detalhes-rota.tsx
+++ app/detalhes-rota.tsx
@@ -1,7 +1,7 @@
 import { obterRotas, Rota } from "@/services/rotas";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function DetalhesRotaScreen() {
@@ -27,32 +27,133 @@ export default function DetalhesRotaScreen() {
         <Text style={styles.title}>Detalhes da Rota</Text>
 
         {rota && (
-          <>
-            <Text style={styles.subtitle}>{rota.motorista}</Text>
+          <View style={styles.card}>
+            <Text style={styles.cardTitulo}>📋 Dados da Rota</Text>
 
-            <Text
-              style={{
-                marginTop: 12,
-                textAlign: "center",
-                color: "#1976D2",
-                fontWeight: "700",
-                fontSize: 22,
-              }}
-            >
-              🚛 {rota.placa}
-            </Text>
+            <View style={styles.linha}>
+              <Text style={styles.label}>👤 Motorista</Text>
+              <Text style={styles.valor}>{rota.motorista}</Text>
+            </View>
 
-            <Text
-              style={{
-                marginTop: 10,
-                textAlign: "center",
-                color: "#666",
-              }}
-            >
-              {rota.dataHoraInicio}
-            </Text>
-          </>
+            <View style={styles.linha}>
+              <Text style={styles.label}>🚛 Placa</Text>
+              <Text style={styles.valor}>{rota.placa}</Text>
+            </View>
+
+            <View style={styles.linha}>
+              <Text style={styles.label}>🆔 ID</Text>
+              <Text style={styles.valor}>VP-{rota.id.slice(-6)}</Text>
+            </View>
+
+            <View style={styles.linha}>
+              <Text style={styles.label}>📅 Início</Text>
+              <Text style={styles.valor}>{rota.dataHoraInicio}</Text>
+            </View>
+
+            <View style={styles.linha}>
+              <Text style={styles.label}>✅ Status</Text>
+              <Text
+                style={{
+                  color: rota.status === "FINALIZADA" ? "#22C55E" : "#1976D2",
+                  fontWeight: "700",
+                }}
+              >
+                {rota.status}
+              </Text>
+            </View>
+          </View>
         )}
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>📷 Foto Inicial</Text>
+
+          {rota?.fotoInicio ? (
+            <Image source={{ uri: rota.fotoInicio }} style={styles.foto} />
+          ) : (
+            <Text style={styles.semFoto}>Nenhuma foto encontrada.</Text>
+          )}
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>📍 Localização Inicial</Text>
+
+          <View style={styles.infoLinha}>
+            <Text style={styles.infoLabel}>Latitude</Text>
+
+            <Text style={styles.infoValor}>
+              {rota?.latitudeInicio?.toFixed(6)}
+            </Text>
+          </View>
+
+          <View style={styles.infoLinha}>
+            <Text style={styles.infoLabel}>Longitude</Text>
+
+            <Text style={styles.infoValor}>
+              {rota?.longitudeInicio?.toFixed(6)}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>📷 Foto Final</Text>
+
+          {rota?.fotoFim ? (
+            <Image source={{ uri: rota.fotoFim }} style={styles.foto} />
+          ) : (
+            <Text style={styles.semFoto}>Rota ainda não foi finalizada.</Text>
+          )}
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>📍 Localização Final</Text>
+
+          <View style={styles.infoLinha}>
+            <Text style={styles.infoLabel}>Latitude</Text>
+
+            <Text style={styles.infoValor}>
+              {rota?.latitudeFim?.toFixed(6)}
+            </Text>
+          </View>
+
+          <View style={styles.infoLinha}>
+            <Text style={styles.infoLabel}>Longitude</Text>
+
+            <Text style={styles.infoValor}>
+              {rota?.longitudeFim?.toFixed(6)}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>⚠️ Ocorrências</Text>
+
+          {rota?.ocorrencias?.length ? (
+            rota.ocorrencias.map((ocorrencia: any, index: number) => (
+              <View key={ocorrencia.id} style={styles.ocorrenciaItem}>
+                <Text style={styles.ocorrenciaTitulo}>
+                  Ocorrência #{index + 1}
+                </Text>
+
+                <Image source={{ uri: ocorrencia.foto }} style={styles.foto} />
+
+                <Text style={styles.infoLabel}>Observação</Text>
+                <Text style={styles.infoValor}>
+                  {ocorrencia.observacao || "Sem observação"}
+                </Text>
+
+                <Text style={styles.infoLabel}>Data</Text>
+                <Text style={styles.infoValor}>{ocorrencia.dataHora}</Text>
+
+                <Text style={styles.infoLabel}>Latitude</Text>
+                <Text style={styles.infoValor}>
+                  {ocorrencia.latitude?.toFixed(6)}
+                </Text>
+
+                <Text style={styles.infoLabel}>Longitude</Text>
+                <Text style={styles.infoValor}>
+                  {ocorrencia.longitude?.toFixed(6)}
+                </Text>
+              </View>
+            ))
+          ) : (
+            <Text style={styles.semFoto}>Nenhuma ocorrência registrada.</Text>
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -75,10 +176,87 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 20,
   },
+  card: {
+    backgroundColor: "#FFF",
+    borderRadius: 18,
+    padding: 20,
+    marginTop: 24,
+    marginBottom: 24,
+    elevation: 3,
+  },
+
+  cardTitulo: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#163A5F",
+    marginBottom: 18,
+  },
+
+  linha: {
+    marginBottom: 16,
+  },
+
+  label: {
+    fontSize: 14,
+    color: "#6B7280",
+  },
+
+  valor: {
+    marginTop: 4,
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#163A5F",
+  },
 
   subtitle: {
     marginTop: 10,
     textAlign: "center",
     color: "#6B7280",
+  },
+  foto: {
+    width: "100%",
+    height: 260,
+    borderRadius: 14,
+    marginTop: 15,
+  },
+
+  semFoto: {
+    marginTop: 15,
+    textAlign: "center",
+    color: "#6B7280",
+  },
+
+  cardTitle: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: "#163A5F",
+  },
+  infoLinha: {
+    marginTop: 16,
+  },
+
+  infoLabel: {
+    fontSize: 15,
+    color: "#6B7280",
+  },
+
+  infoValor: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#163A5F",
+    marginTop: 4,
+  },
+  ocorrenciaItem: {
+    marginTop: 20,
+    paddingTop: 20,
+    borderTopWidth: 1,
+    borderTopColor: "#E5E7EB",
+  },
+
+  ocorrenciaTitulo: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#163A5F",
+    marginBottom: 12,
   },
 });
